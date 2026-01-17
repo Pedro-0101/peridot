0156069087b9dd9748e0de536f7087fad446944e
Pedro-0101 <pedro.tiete@outlook.com>
2026-01-16 22:04:25 -0300

    feat: criado endpoint de saude

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	slog.Info("Iniciando api...")
+
+	server := gin.Default()
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.Run(":8008")
 }
